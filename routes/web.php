872e26bb018ab3267f84bcd7d4d8e76b69337580
hirokosaito310 <shimajima007@icloud.com>
2024-09-11 14:28:27 +0900

    ルーティング設定完了

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,11 +9,21 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
-*/
+  */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('memos', MemoController::class);
+ // リソースを使用しない場合
+ // Route::get('/memos', [MemoController::class, 'index']);
+ // Route::get('/memos/create', [MemoController::class, 'create']);
+ // Route::post('/memos', [MemoController::class, 'store']);
+ // Route::get('/memos/{memo}', [MemoController::class, 'show']);
+ // Route::get('/memos/{memo}/edit', [MemoController::class, 'edit']);
+ // Route::patch('/memos/{memo}', [MemoController::class, 'update']);
+ // Route::delete('/memos/{memo}', [MemoController::class, 'destroy']);
