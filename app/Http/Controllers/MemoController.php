f2c282128d573eda151f17e7e6058668f887997b
hirokosaito310 <shimajima007@icloud.com>
2024-09-11 17:42:39 +0900

    登録機能作成(未了)

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -5,17 +5,37 @@ namespace App\Http\Controllers;
 use App\Models\Memo;
 use Illuminate\Http\Request;
 
+use function PHPUnit\Framework\directoryExists;
+
 class MemoController extends Controller
 {
     public function index()
     {
         $memos = Memo::all();
-        return view ('memos.index',['memos' => $memos]);
+        return view('memos.index', ['memos' => $memos]);
     }
-    
+
+    public function create()
+    {
+        return view('memos.create');
+    }
+
+    public function store(Request $request)
+    {
+        // インスタンスの作成
+        $memo = new Memo;
+
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        $memo->save();
+
+        return redirect('/memos');
+    }
+
     public function show($id)
     {
         $memo = Memo::find($id);
-        return view ('memos.show',['memo' => $memo]);
+        return view('memos.show', ['memo' => $memo]);
     }
 }
