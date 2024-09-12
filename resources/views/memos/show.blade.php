6325048579ff776a0cbd8b3d61cfc15f49488bdf
hirokosaito310 <shimajima007@icloud.com>
2024-09-12 09:33:16 +0900

    編集機能作成

--- resources/views/memos/show.blade.php
+++ resources/views/memos/show.blade.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>memo show</title>
 </head>
+
 <body>
     <a href="{{ route('memos.index') }}">戻る</a>
     <h1>{{ $memo->title }}</h1>
-    <p>{!!  nl2br(e($memo->body)) !!}</p>
+    <p>{!! nl2br(e($memo->body)) !!}</p>
+    <button onclick='location.href="{{ route('memos.edit', $memo) }}"'>編集する</button>
 </body>
+
 </html>
